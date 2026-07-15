12efa69c0aa11fbec21912553bb23842cae57341
swapnilpatil1460 <swapnilpatil1460@gmail.com>
2026-07-15 07:01:57 +0530

    fix: switch pdf generation to native window.print for perfect tailwind rendering

--- src/hooks/usePdfDownloader.ts
+++ src/hooks/usePdfDownloader.ts
@@ -1,19 +1,11 @@
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 const usePdfDownloader = () => {
   const handleDownloadPDF = (
     contentToPrint: HTMLDivElement,
     fileName: string
   ) => {
     if (contentToPrint) {
-      html2canvas(contentToPrint).then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF();
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-        pdf.save(fileName + ".pdf");
-      });
+      document.title = fileName;
+      window.print();
     } else {
       console.error("Content not found");
     }
