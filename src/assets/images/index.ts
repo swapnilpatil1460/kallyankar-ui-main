cf38754bbe6e6199a13342b85bee73ce346cdd9e
swapnilpatil1460 <swapnilpatil1460@gmail.com>
2026-07-17 21:24:29 +0530

    feat: add user-uploaded certificate and stack images to the slider

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,3 +4,5 @@ export { default as LandingPageBattery } from "./battery_landing_page.jpg";
 export { default as KB_1 } from "./kb-1.jpg";
 export { default as KB_2 } from "./kb-2.jpg";
 export { default as KB_3 } from "./kb-3.jpg";
+export { default as KB_Stack } from "./stack.jpg";
+export { default as KB_Certificate } from "./certificate..jpg";
